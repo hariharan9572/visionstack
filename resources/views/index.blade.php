204a33ba78856c25131a43d748ef1f2e751e0064
Hari <hariharan9572@gmail.com>
2025-03-31 02:10:05 +0530

    Logo link fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -952,7 +952,7 @@
     <div class="container footer-top">
         <div class="row gy-4">
             <div class="col-lg-4 col-md-6 footer-about">
-                <a href="index.html" class="logo d-flex align-items-center">
+                <a href="{{ url('/') }}" class="logo d-flex align-items-center">
                     <span class="sitename transparent-text">Visionstack</span>
                 </a>
                 <div class="footer-contact pt-3">
