c3771775523ee648fd7d66516296418d259ddb54
Hari <hariharan9572@gmail.com>
2025-04-07 22:09:18 +0530

    Designed by & Distributed by removed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1012,7 +1012,7 @@
             <!-- You can delete the links only if you've purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-            Designed by <a href="https://Visionstack.in/">Visionstack</a> Distributed by <a href="https://Visionstack.in">Visionstack</a>
+{{--            Designed by <a href="https://Visionstack.in/">Visionstack</a> Distributed by <a href="https://Visionstack.in">Visionstack</a>--}}
         </div>
     </div>
 
