f6987b11ecd2403064496276a66badb2deb52537
Hari <hariharan9572@gmail.com>
2025-04-18 07:18:12 +0530

    content update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,16 +84,16 @@
     <!-- Hero Section -->
     <section id="hero" class="hero section dark-background">
 
-        <img src="assets/img/galaxy-nature-aesthetic-background-starry-sky-mountain-remixed-media.jpg" alt="" data-aos="fade-in">
+        <img src="assets/img/galaxy-nature-aesthetic-background-starry-sky-mountain-remixed-media.png" alt="" data-aos="fade-in">
 
-        <div class="container d-flex flex-column align-items-center">
+        {{-- <div class="container d-flex flex-column align-items-center">
             <h2 data-aos="fade-up" data-aos-delay="100"><span class="typed transparent-text" data-typed-items="Visionstack, Website Development, UI/UX Design, Digital Marketing, Enterprise SaaS Solutions, Cloud Applications, Custom Software, Digital Transformation, API Integrations"></span></h2>
             <p data-aos="fade-up" data-aos-delay="200">Transforming businesses with innovative software solutions and SaaS platforms</p>
             <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
                 <a href="#about" class="btn-get-started">Get Started</a>
                 <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
             </div>
-        </div>
+        </div> --}}
 
     </section>
     <!-- /Hero Section -->
